80a5d056c9befda8cfdba481caf67396f2987811
ege-eker <ege071997@gmail.com>
2025-05-07 23:29:39 +0300

    Setting up router

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"RestAPI-todo-app/controllers"
+	"RestAPI-todo-app/handler"
 	"RestAPI-todo-app/middlewares"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
+	// Initialize the Gin router
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/**/*")
 
-	r.GET("/login", controllers.LoginPage)
-	r.POST("/login", controllers.Login)
-	r.POST("/logout", controllers.Logout)
+	// API routes
+	api := r.Group("/api")
+	api.POST("/login", handler.Login)
 
-	r.GET("/", middlewares.AuthMiddleware(), func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
+	// API routes that require authentication middleware
+	auth := api.Group("/", middlewares.AuthMiddleware())
 
+	auth.GET("/todo-lists", handler.GetTodos)
+	auth.POST("/todo-lists", handler.AddTodoList)
+	auth.PUT("/todo-lists/:id", handler.RenameTodoList)
+	auth.DELETE("/todo-lists/:id", handler.DeleteTodoList)
+	auth.POST("/todo-lists/:id/step", handler.AddTodoStep)
+	auth.DELETE("/todo-lists/:id/step/:step_id", handler.DeleteTodoStep)
+	auth.PUT("/todo-lists/:id/step/:step_id", handler.RenameTodoStep)
+	auth.POST("/todo-lists/:id/step/:step_id/toggle", handler.ToggleStepCompletion)
+
+	// Gets Todo lists with deleted elements, only for admins. uncomment if needed
+	//auth.GET("/admin/todolists", middlewares.AuthMiddleware(), handler.GetAllTodosAdmin)
+
+	// Start the server on port 8080
 	r.Run(":8080")
 }
